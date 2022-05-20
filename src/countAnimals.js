3fa173ca7c7adbb4d2e53d6f429715804371596e
Sally Diament <sally.barzilai@gmail.com>
2022-05-20 18:55:19 -0300

    incluindo o requisito 4

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -4,4 +4,10 @@ function countAnimals(animal) {
   // seu código aqui
 }
 
+// const residentes = data.species[0].
+
+// { species: { residents: residentes } } = data;
+
+// console.log(residentes.length);
+
 module.exports = countAnimals;
