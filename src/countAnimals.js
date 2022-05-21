58530b284ae2f05ae5aa9e15a9056717847a0e9c
Sally Diament <sally.barzilai@gmail.com>
2022-05-21 17:01:16 -0300

    incluindo o requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,13 +1,22 @@
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  if (animal === undefined) {
+    return data.species.reduce((acc, curr) => {
+      acc[curr.name] = curr.residents.length;
+      return acc;
+    }, {});
+  }
+  if (animal.specie !== undefined && animal.sex === undefined) {
+    const re = data.species.filter((e) => animal.specie === e.name).map((e) => e.residents.length);
+    return re[0];
+  }
+  const objResidentes = data.species.filter((e) => animal.specie === e.name)[0].residents;
+  const arrayRetorno = objResidentes.filter((residente) => animal.sex === residente.sex);
+  return arrayRetorno.length;
 }
 
-// const residentes = data.species[0].
-
-// { species: { residents: residentes } } = data;
-
-// console.log(residentes.length);
+// console.log(countAnimals());
+// console.log(countAnimals({ specie: 'elephants', sex: 'male' }));
 
 module.exports = countAnimals;
