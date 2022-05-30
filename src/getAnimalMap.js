cdd5d0e510d908219ab9b6e7179265fad4712c67
Sally Diament <sally.barzilai@gmail.com>
2022-05-29 22:09:05 -0300

    incluindo uma parte do requisito 14

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,25 +1,96 @@
-/* eslint-disable complexity */
 const data = require('../data/zoo_data');
 
-function getAnimalMap(options) {
-  // const chavesOptions = Object.keys(options);
-  // switch (options) {
-  // case options === undefined:
-  //   return 'primeiro caso';
-  //   break;
-  // case chavesOptions.length === 1 && chavesOptions[0] !== 'includeNames':
-  //   return 'primeiro caso';
-  // case chavesOptions.length === 1 && chavesOptions[0] === 'includeNames':
-  //   return 'segundo caso';
-  // case chavesOptions.length === 2 && chavesOptions[1] === 'sorted':
-  //   return 'terceiro caso';
-  // case chavesOptions.length === 2 && chavesOptions[1] === 'sex':
-  //   return 'quarto caso';
-  // case chavesOptions.length === 3:
-  //   return 'quinto caso';
-  // default:
-  //   break;
-  // }
+function mapInicial() {
+  return data.species.reduce((acc, specie) => {
+    acc[specie.location].push(specie.name);
+    return acc;
+  }, { NE: [], NW: [], SE: [], SW: [] });
 }
+
+// console.log(mapInicial());
+
+function mapComNomeDosAnimais() {
+  return data.species.reduce((acc, specie) => {
+    acc[specie.location].push({ [specie.name]: specie.residents.map((resident) => resident.name) });
+    return acc;
+  }, { NE: [], NW: [], SE: [], SW: [] });
+}
+
+// console.log(mapComNomeDosAnimais());
+
+function mapComNomeDosAnimaisSort() {
+  return data.species.reduce((acc, specie) => {
+    acc[specie.location].push({ [specie.name]: (specie.residents.map((resident) => resident.name)).sort() });
+    return acc;
+  }, { NE: [], NW: [], SE: [], SW: [] });
+}
+
+// console.log(mapComNomeDosAnimaisSort().NE);
+
+function mapComNomeDosAnimaisSexFemale(options) {
+  return data.species.reduce((acc, specie) => {
+    acc[specie.location].push({ [specie.name]: specie.residents.filter((resident) => resident.sex === options.sex).map((female) => female.name) });
+    return acc;
+  }, { NE: [], NW: [], SE: [], SW: [] });
+}
+
+// console.log(mapComNomeDosAnimaisSexFemale({ sex: 'female' }).NW);
+
+function mapComNomeDosAnimaisSexFemaleSorted(options) {
+  return data.species.reduce((acc, specie) => {
+    acc[specie.location].push({ [specie.name]: specie.residents.filter((resident) => {
+      return resident.sex === options.sex;
+    }).map((female) => female.name).sort() });
+    return acc;
+  }, { NE: [], NW: [], SE: [], SW: [] });
+}
+
+// console.log(mapComNomeDosAnimaisSexFemaleSorted({ sex: 'female' }).NW);
+
+function tresParam(options) {
+  if (options.includeNames === true && options.sorted === true && options.sex === 'female') {
+    return mapComNomeDosAnimaisSexFemaleSorted(options);
+  }
+}
+
+function includesNameTrueESexFemale(options) {
+  if (options.includeNames === true && options.sorted === true) {
+    return mapComNomeDosAnimaisSort();
+  }
+}
+
+// function includesNameTrueSortedTrue(options) {
+//   if (options.includeNames === true && options.sorted === true) {
+//     return mapComNomeDosAnimaisSort();
+//   }
+// }
+
+// function includesNameTrue(options) {
+//   if (options.includeNames === true) {
+//     return mapComNomeDosAnimais();
+//   }
+// }
+
+// console.log(includesNameTrue());
+
+function getAnimalMap(options = 'todos') {
+  // if (options.includeNames === true && options.sorted === true && options.sex === 'female') {
+  //   return mapComNomeDosAnimaisSexFemaleSorted(options)
+  // }
+  // if (options.includeNames === true && options.sorted === true) {
+  //   return mapComNomeDosAnimaisSort();
+  // }
+  tresParam(options);
+  includesNameTrueESexFemale(options);
+  if (options.includeNames === true && options.sex === 'female') {
+    return mapComNomeDosAnimaisSexFemale(options);
+  }
+  if (options.includeNames === true) {
+    return mapComNomeDosAnimais();
+  }
+  return mapInicial();
+}
+
+// console.log(getAnimalMap({ includeNames: true, sex: 'female' }));
 
 module.exports = getAnimalMap;
